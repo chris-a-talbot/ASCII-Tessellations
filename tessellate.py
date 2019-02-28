2a95e073eab45cb6808826f4c8a7c99e4ff20f6d
chris-a-talbot <christopher.a.talbot@gmail.com>
2019-02-28 10:56:03 -0500

    Completed rotation functions. Need to find a way to gather them and turn them into a tessellation.

--- tessellate.py
+++ tessellate.py
@@ -35,35 +35,38 @@ def to_matrix(pattern, size):
 # Iterating through the matrix; testing each character to see if it is in a rotation dictionary; if it is, setting it to its rotated version
 def rotate_characters(matrix, degrees):
     if degrees == 180:
-        for object in matrix:
-            return
+        for index_o, object in enumerate(matrix):
+            for index_i, item in enumerate(object):
+                if item in rot180:
+                    matrix[index_o][index_i] = rot180.get(item)
     elif degrees == 90:
         for index_o, object in enumerate(matrix):
             for index_i, item in enumerate(object):
                 if item in rot90:
                     matrix[index_o][index_i] = rot90.get(item)
-        return matrix
+    return matrix
 
-# Needs to take a single matrix and a. rotate the ascii characters x degrees, b. rotate the matrix x degrees
-# Potential input: Matrix, degrees of turn
+# Takes a matrix (in this case, a numpy chararray and rotates it given degrees)
+# Numpy rot90 function rotates an array 90 degrees, but it seems the original post wants a -90 degree turn; because of this, I set a 90 degree turn to actually turn 270 degrees.
+# If degrees are not a valid option or do not need any change (0 degrees), matrix returns as it came in
 def rotate(matrix, degrees):
     if degrees == 90:
+        matrix = np.rot90(matrix, 3)
+    elif degrees == 180:
+        matrix = np.rot90(matrix, 2)
+    elif degrees == 270 or degrees == -90:
         matrix = np.rot90(matrix)
     return matrix
 
-# Needs to take a matrix, gather the necessary rotations of that matrix, put them together, and output the entire thing
-# Potential input: Matrix, degrees of turn, number of times to tessellate
-def tessellate():
-    return
-
-# Test function to gather input and put it through the other functions
-def get_input(data, repeat):
+# Needs to take a matrix, rotate the matrix as much as necessary, put all of the necessary matrixes together, and output it as a set of strings
+# Takes user input. Data variable takes: Rotation in degrees, size of the square matrix, and the pattern of the matrix. Repeat takes the number of times to repeat the process.
+# This part is a WIP.
+def tessellate(data, repeat):
     rotation, size, *pattern = data.split("\n")
     matrix = rotate(rotate_characters(to_matrix(pattern, size), 90), 90)
-    print(pattern)
-    print(matrix)
+    return
 
-get_input("""90
+tessellate("""90
 4
 ####
 #--#
