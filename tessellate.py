1d95ca7b3b37c51bc2dceea0a82dc658c7ba27f9
chris-a-talbot <christopher.a.talbot@gmail.com>
2019-02-27 20:35:16 -0500

    Added ASCII conversions & plan for functions

---
+++ tessellate.py
@@ -0,0 +1,32 @@
+# Dictionary with the 90 degree turned version of applicable ascii characters
+rot90 = {
+    "/": "\\",
+    "\\": "/",
+    "-": "|",
+    "|": "-",
+}
+
+# Dictionary with the 180 degree turned version of applicable ascii characters
+rot180 = {
+    "!": "¡", "!": "!",
+    "&": "⅋", "⅋": "&",
+    "(": ")", ")": "(",
+    ",": "`", "`": ",",
+    "'": ".", ".": "'",
+    ";": "؛", "؛": ";",
+    "<": ">", ">": "<",
+    "?": "¿", "¿": "?",
+    "[": "]", "]": "[",
+    "^": "v", "v": "^",
+    "{": "}", "}": "{",
+}
+
+# Needs to take a single matrix and a. rotate the ascii characters x degrees, b. rotate the matrix x degrees
+# Potential input: Matrix, degrees of turn
+def rotate():
+    return
+
+# Needs to take a matrix, gather the necessary rotations of that matrix, put them together, and output the entire thing
+# Potential input: Matrix, degrees of turn, number of times to tessellate
+def tessellate():
+    return
